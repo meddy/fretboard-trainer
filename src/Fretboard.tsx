81441534d376db492a9be905bfeca8dc5795bba6
Max Eddy <maxiscool@gmail.com>
2020-10-25 12:41:08 -0700

    More bootstrap.

--- src/Fretboard.tsx
+++ src/Fretboard.tsx
@@ -6,12 +6,24 @@ export default function Fretboard() {
   return (
     <div className={styles.container}>
       <div className={styles.info}>
-        <div>(1) E</div>
-        <div>(2) B</div>
-        <div>(3) G</div>
-        <div>(4) D</div>
-        <div>(5) A</div>
-        <div>(6) E</div>
+        <div>
+          (1)<span className={styles["info-note"]}>E</span>
+        </div>
+        <div>
+          (2)<span className={styles["info-note"]}>B</span>
+        </div>
+        <div>
+          (3)<span className={styles["info-note"]}>G</span>
+        </div>
+        <div>
+          (4)<span className={styles["info-note"]}>D</span>
+        </div>
+        <div>
+          (5)<span className={styles["info-note"]}>A</span>
+        </div>
+        <div>
+          (6)<span className={styles["info-note"]}>E</span>
+        </div>
       </div>
       <div className={styles.fretboard}>
         <div className={styles.nut} />
--- src/NoteSelector.tsx
+++ src/NoteSelector.tsx
@@ -1,44 +1,85 @@
 import React, { useState } from "react";
-import { Button, ButtonGroup } from "reactstrap";
+import { Button, ButtonGroup, Form } from "reactstrap";
+
+enum Note {
+  A = "A",
+  B = "B",
+  C = "C",
+  D = "D",
+  E = "E",
+  F = "F",
+  G = "G",
+}
+
+enum Accidental {
+  SHARP = "sharp",
+  FLAT = "flat",
+}
 
 export default function NoteSelector() {
-  const [note, setSelectedNote] = useState<string | null>(null);
+  const [note, setSelectedNote] = useState<Note | null>(null);
+  const [accidental, setAccidental] = useState<Accidental | null>(null);
 
   return (
-    <>
+    <Form inline>
       <ButtonGroup>
-        <Button active={note === "A"} onClick={() => setSelectedNote("A")}>
-          A
+        <Button
+          active={note === Note.A}
+          onClick={() => setSelectedNote(Note.A)}
+        >
+          {Note.A}
         </Button>
-        <Button active={note === "B"} onClick={() => setSelectedNote("B")}>
-          B
+        <Button
+          active={note === Note.B}
+          onClick={() => setSelectedNote(Note.B)}
+        >
+          {Note.B}
         </Button>
-        <Button active={note === "C"} onClick={() => setSelectedNote("C")}>
-          C
+        <Button
+          active={note === Note.C}
+          onClick={() => setSelectedNote(Note.C)}
+        >
+          {Note.C}
         </Button>
-        <Button active={note === "D"} onClick={() => setSelectedNote("D")}>
-          D
+        <Button
+          active={note === Note.D}
+          onClick={() => setSelectedNote(Note.D)}
+        >
+          {Note.D}
         </Button>
-        <Button active={note === "E"} onClick={() => setSelectedNote("E")}>
-          E
+        <Button
+          active={note === Note.E}
+          onClick={() => setSelectedNote(Note.E)}
+        >
+          {Note.E}
         </Button>
-        <Button active={note === "F"} onClick={() => setSelectedNote("F")}>
-          F
+        <Button
+          active={note === Note.F}
+          onClick={() => setSelectedNote(Note.F)}
+        >
+          {Note.F}
         </Button>
-        <Button active={note === "G"} onClick={() => setSelectedNote("G")}>
-          G
+        <Button
+          active={note === Note.G}
+          onClick={() => setSelectedNote(Note.G)}
+        >
+          {Note.G}
         </Button>
       </ButtonGroup>
-      <div>
-        <label>
-          <input id="flat" name="accidental" type="radio" value="flat" />
+      <ButtonGroup>
+        <Button
+          active={accidental === Accidental.SHARP}
+          onClick={() => setAccidental(Accidental.SHARP)}
+        >
           &#9839;
-        </label>
-        <label>
-          <input id="sharp" name="accidental" type="radio" value="sharp" />
+        </Button>
+        <Button
+          active={accidental === Accidental.FLAT}
+          onClick={() => setAccidental(Accidental.FLAT)}
+        >
           &#9837;
-        </label>
-      </div>
-    </>
+        </Button>
+      </ButtonGroup>
+    </Form>
   );
 }
