fd51ad23dd50e22293b487f00b5887164affb6c6
Max Eddy <maxiscool@gmail.com>
2020-10-31 14:06:09 -0700

    WIP: dynamic fretboard.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "./App.module.css";
 import Fretboard from "./Fretboard";
 import NoteSelector from "./NoteSelector";
 
+function getRandomInt(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomNotePosition() {
+  return {
+    string: getRandomInt(1, 6),
+    fret: getRandomInt(0, 12),
+  };
+}
+
 function App() {
   return (
     <div className={styles.container}>
-      <Fretboard />
+      <Fretboard {...randomNotePosition()} />
       <NoteSelector />
     </div>
   );
--- src/Fretboard.tsx
+++ src/Fretboard.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import clsx from "clsx";
 
 import styles from "./Fretboard.module.css";
 
-export default function Fretboard() {
+interface FretboardProps {
+  string: number;
+  fret: number;
+}
+
+const strings = [1, 2, 3, 4, 5];
+const frets = Array.from(Array(13).keys());
+const inlays = [
+  { string: 2, fret: 12 },
+  { string: 3, fret: 3 },
+  { string: 3, fret: 5 },
+  { string: 3, fret: 7 },
+  { string: 3, fret: 9 },
+  { string: 4, fret: 12 },
+];
+
+export default function Fretboard(props: FretboardProps) {
   return (
     <div className={styles.container}>
       <div className={styles.info}>
@@ -26,95 +43,50 @@ export default function Fretboard() {
         </div>
       </div>
       <div className={styles.fretboard}>
-        <div className={styles.nut} />
-        {/* 1st */}
-        <div className={styles.fret}>
-          <div className={styles.note} />
-        </div>
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret}>
-          <div className={styles.note} />
-        </div>
-        <div className={styles.fret}>
-          <div className={styles.note} />
-        </div>
-        {/* 2nd */}
-        <div className={styles.nut} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret}>
-          <div className={`${styles.inlay} ${styles["inlay-top"]}`} />
-        </div>
-        {/* 3rd */}
-        <div className={styles.nut} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret}>
-          <div className={styles.inlay} />
-        </div>
-        <div className={styles.fret} />
-        <div className={styles.fret}>
-          <div className={styles.inlay} />
-        </div>
-        <div className={styles.fret} />
-        <div className={styles.fret}>
-          <div className={styles.inlay} />
-        </div>
-        <div className={styles.fret} />
-        <div className={styles.fret}>
-          <div className={styles.inlay} />
-        </div>
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        {/* 4th */}
-        <div className={styles.nut} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret}>
-          <div className={`${styles.inlay} ${styles["inlay-bottom"]}`} />
-        </div>
-        {/* 5th */}
-        <div className={styles.nut} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        <div className={styles.fret} />
-        {/* 6th */}
+        {strings.map((string) =>
+          frets.map((fret) => {
+            const match = props.string === string && props.fret === fret;
+            if (fret === 0) {
+              return (
+                <div
+                  className={clsx({
+                    [styles.note]: match,
+                    [styles["open-note"]]: match,
+                    [styles["sixth-string-note"]]: props.string === 6,
+                  })}
+                  key={`${string}-${fret}`}
+                />
+              );
+            }
+
+            return (
+              <div className={styles.fret} key={`${string}-${fret}`}>
+                {match && (
+                  <div
+                    className={clsx({
+                      [styles.note]: true,
+                      [styles["sixth-string-note"]]: props.string === 6,
+                    })}
+                  />
+                )}
+                {inlays
+                  .filter(
+                    (inlay) => string === inlay.string && fret === inlay.fret
+                  )
+                  .map(() => (
+                    <div
+                      className={clsx({
+                        [styles.inlay]: true,
+                        [styles["inlay-top"]]: string === 2,
+                        [styles["inlay-bottom"]]: string === 4,
+                      })}
+                      key={`${string}-${fret}-inlay`}
+                    />
+                  ))}
+              </div>
+            );
+          })
+        )}
       </div>
     </div>
   );
